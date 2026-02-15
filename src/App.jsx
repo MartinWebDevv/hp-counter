80373df257b350ce9c7e2ee0887ece4517cc5479
Martin <mc55jr@gmail.com>
2026-02-15 02:26:43 -0800

    adding a tons of mechanics

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Index from "./components";
 import HPCounter from "./components/HPCounter";
+import Holder from "./components/holder"
 
 function App() {
 
   return (
     <div>
       <HPCounter />
+      {/* <Holder /> */}
     </div>
+
   )
 }
 
